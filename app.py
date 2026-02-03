ddc584db6c6042e11cedc59fcd4577e3bb1f3361
boat-s-1 <marron.toyohashi@gmail.com>
2026-02-03 22:02:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ with tab1:
             percent = s / total_score * 100
 
         # おすすめ度が30%以上なら本命扱い
-is_double = percent >= 30
+　　　　　　is_double = percent >= 30
 
 
         show_rank_card(
@@ -296,6 +296,7 @@ with tab3:
             file_name="boat_prediction.png",
             mime="image/png"
         )
+
 
 
 
